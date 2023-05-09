50623306ee4d7af92764566554989beea3697cef
Noel <noelhorta88@outlook.com>
2023-05-08 18:12:54 -0600

    portfolio_v1.1

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: '/Beccar.pdf',
+            href: "https://drive.google.com/file/d/1NgPku1FpyItkY6HoO6plz2tADuo2NPy0/view?usp=share_link",
             style: "rounded-br-md",
             download: true,
         }
